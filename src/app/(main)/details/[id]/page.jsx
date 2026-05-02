ebda0075df99c86cdefec37a87717ad4782b497f
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-05-02 17:37:32 +0600

    2nd commit

--- src/app/(main)/details/[id]/page.jsx
+++ src/app/(main)/details/[id]/page.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import data from '@/data/data.json';
+import Link from 'next/link';
 
 const Details = async({ params }) => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
     const { id } = await params;
 const course = data.find(course => course.id === parseInt(id));
     return (
         <div>
             {course && (
-                <div className='flex justify-center items-center flex-col'>
+                <div className='flex justify-center items-center flex-col  bg-slate-200 rounded-3xl p-5 space-y-3 mt-5 container mx-auto'>
                     <img className='mx-auto object-cover' src={course.image} alt={course.title} width={300} height={200} />
-                    <h1>{course.title}</h1>
-                    <p>{course.description}</p>
-                    <p>Duration: {course.duration}</p>
-                    <p>Level: {course.level}</p>
-                    <p>Rating: {course.rating}</p>
+                    <h1 className='font-bold text-xl'>{course.title}</h1>
+                    <p className='font-bold text-xl'>{course.description}</p>
+                    <p className='font-bold text-xl'>Duration: {course.duration}</p>
+                    <p className='font-bold text-xl'>Level: {course.level}</p>
+                    <p className='font-bold text-xl'>Rating: {course.rating}</p>
+                    <p className='text-center text-xl'>Details: {course.details}</p>
+                    <Link href="/" className='btn btn-primary mt-5'>Go Back Home</Link>
                 </div>
             )}
         </div>
--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,13 +1,16 @@
 import Hero from "@/component/Hero";
 import Instructor from "@/component/Instructor";
+import Newrls from "@/component/Newrls";
 import Tips from "@/component/Tips";
 
 export default function Home() {
   return (
     <div>
       <Hero></Hero>
+      <Newrls></Newrls>
       <Tips></Tips>
       <Instructor></Instructor>
+
 
 
     </div>
